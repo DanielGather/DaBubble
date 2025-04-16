c481878f2f35540b6a3a3beaafa174caa0f9fc49
Loew3nzahn <gather.daniel@yahoo.de>
2025-04-16 16:41:19 +0200

    npm install

--- DaBubble/src/app/components/firestore-test/firestore-test.component.ts
+++ DaBubble/src/app/components/firestore-test/firestore-test.component.ts
@@ -2,26 +2,22 @@ import { Component, Inject, inject } from '@angular/core';
 import { FirestoreService } from '../../services/firestore.service';
 import { CommonModule } from '@angular/common';
 
-
 @Component({
   selector: 'app-firestore-test',
   imports: [CommonModule],
   templateUrl: './firestore-test.component.html',
-  styleUrl: './firestore-test.component.scss'
+  styleUrl: './firestore-test.component.scss',
 })
 export class FirestoreTestComponent {
-
   /**
    * firstoreService variable
    */
   firestoreService: FirestoreService = inject(FirestoreService);
-  
+
   /**
    * variable to access the data of the collection
    */
   collection$ = this.firestoreService.getCollectionData('test-collection');
 
   constructor() {}
-
-
 }
