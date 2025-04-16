dd34fa505082000b39199447dad9d1f561d4904a
andre <andre.karyoti.schuro@gmail.com>
2025-04-16 10:58:10 +0200

    initialized firestore db | deleted the placeholder | created a folder named components | create a firestore-test component | implemented firestore-test component into (app.component.ts/app.component.html)

--- DaBubble/src/app/app.component.ts
+++ DaBubble/src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FirestoreTestComponent } from './components/firestore-test/firestore-test.component'; //test
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    FirestoreTestComponent //test
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ DaBubble/src/app/components/firestore-test/firestore-test.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-firestore-test',
+  imports: [],
+  templateUrl: './firestore-test.component.html',
+  styleUrl: './firestore-test.component.scss'
+})
+export class FirestoreTestComponent {
+
+}
