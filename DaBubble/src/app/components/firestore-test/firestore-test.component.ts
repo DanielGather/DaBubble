a4d202a8e5002146893ee77467cfee6fff576f84
andre <andre.karyoti.schuro@gmail.com>
2025-04-16 13:42:18 +0200

    added a realtime getCollectionData-function (readonly) to firestore service | created a usecase example on firestore-test.component to demonstrate how to use the connection through async pipe

--- DaBubble/src/app/components/firestore-test/firestore-test.component.ts
+++ DaBubble/src/app/components/firestore-test/firestore-test.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, Inject, inject } from '@angular/core';
+import { FirestoreService } from '../../services/firestore.service';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-firestore-test',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './firestore-test.component.html',
   styleUrl: './firestore-test.component.scss'
 })
 export class FirestoreTestComponent {
+
+  /**
+   * firstoreService variable
+   */
+  firestoreService: FirestoreService = inject(FirestoreService);
+  
+  /**
+   * variable to access the data of the collection
+   */
+  collection$ = this.firestoreService.getCollectionData('test-collection');
+
+  constructor() {}
+
 
 }
--- DaBubble/src/app/services/firestore.service.ts
+++ DaBubble/src/app/services/firestore.service.ts
@@ -1,9 +1,37 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { Firestore, collection, collectionData } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+Observable
 
 @Injectable({
   providedIn: 'root'
 })
 export class FirestoreService {
 
-  constructor() { }
+  /**
+   * firestore service
+   */
+  firestore: Firestore = inject(Firestore);
+
+  constructor() {}
+
+  /**
+   * this function returns the specific collection-reference of the firestore database.
+   * 
+   * @returns collection-reference
+   */
+  getCollectionRef(collectionKey:string) {
+    return collection(this.firestore,`${collectionKey}`);
+  }
+
+  /**
+   * 
+   * @param collectionKey the name/key of the collection
+   * @returns the collection itself. it contains a list of documents.
+   */
+  getCollectionData(collectionKey:string) {
+    return collectionData(this.getCollectionRef(collectionKey));
+  }
+  
+
 }
