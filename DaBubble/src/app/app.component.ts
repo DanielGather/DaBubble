55d187cbe4b3de6db42f99a52975fc14eb1f4a7f
TypingAndy <andreas.georg@outlook.com>
2025-04-21 17:12:19 +0200

    move shared folder to components

--- DaBubble/src/app/app.component.ts
+++ DaBubble/src/app/app.component.ts
@@ -1,20 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FirestoreTestComponent } from './components/firestore-test/firestore-test.component'; //test
-import { HeaderComponent } from './shared/header/header.component';
-import { SearchbarComponent } from './shared/searchbar/searchbar.component';
-
 
 @Component({
   selector: 'app-root',
   imports: [
     RouterOutlet,
     FirestoreTestComponent, //test
-    HeaderComponent,
-    SearchbarComponent
   ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'DaBubble';
