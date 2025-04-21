b88e45f5d0b7f70bbc9c462f0ba31f254f672691
TypingAndy <andreas.georg@outlook.com>
2025-04-21 17:14:35 +0200

    module integration

--- DaBubble/src/app/app.component.ts
+++ DaBubble/src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FirestoreTestComponent } from './components/firestore-test/firestore-test.component'; //test
+import { HeaderComponent } from './components/shared/header/header.component';
 
 @Component({
   selector: 'app-root',
   imports: [
     RouterOutlet,
     FirestoreTestComponent, //test
+    HeaderComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
