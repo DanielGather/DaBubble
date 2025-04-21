3b85d202888f6956dfb4ddd591e0db856f373b18
andre <andre.karyoti.schuro@gmail.com>
2025-04-22 00:01:01 +0200

    deleted update function from content element

--- DaBubble/src/app/app.routes.ts
+++ DaBubble/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { PrivateChatComponent } from './components/private-chat/private-chat.component';
+PrivateChatComponent
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'a-private-chat-variable', component:  PrivateChatComponent}
+];
