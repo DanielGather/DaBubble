4916708ccaf289c51dec40828ff5d30b39edb2c5
Loew3nzahn <gather.daniel@yahoo.de>
2025-04-22 10:15:13 +0200

    added variables to tailwind

--- DaBubble/tailwind.config.js
+++ DaBubble/tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "bg-main": "#eceefe",
+        "bg-component": "#ffffff",
+        "bg-button": "#444df2",
+      },
+      fontSize: {
+        24: "24px",
+        fontSizeChat: "18px",
+      },
+      borderRadius: {
+        standard: "1.875rem",
+      },
+    },
   },
   plugins: [],
 };
