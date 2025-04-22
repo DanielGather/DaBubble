3421504825479100fbefae77a765479ae918cb4d
TypingAndy <andreas.georg@outlook.com>
2025-04-22 14:16:44 +0200

    added color purple

--- DaBubble/tailwind.config.js
+++ DaBubble/tailwind.config.js
@@ -7,6 +7,7 @@ module.exports = {
         "bg-main": "var(--bg-main)",
         "bg-component": "var(--bg-component)",
         "bg-button": "var(--bg-button)",
+        "fc-purple": "var(--font-color-purple)",
       },
       fontSize: {
         24: "var(--font-size-bold)",
