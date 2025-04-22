764c3fd4ab4037a436e54bd47be9ba6a190784d1
andre <andre.karyoti.schuro@gmail.com>
2025-04-22 02:08:28 +0200

    added private chat component | added chat-messages component | added chat-input component

--- DaBubble/src/app/components/private-chat/private-chat.component.ts
+++ DaBubble/src/app/components/private-chat/private-chat.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { ChatInputComponent } from '../shared/chat-input/chat-input.component';
+import { ChatMessagesContainerComponent } from '../shared/chat-messages-container/chat-messages-container.component';
 
 @Component({
   selector: 'app-private-chat',
-  imports: [],
+  imports: [
+    ChatInputComponent,
+    ChatMessagesContainerComponent
+  ],
   templateUrl: './private-chat.component.html',
   styleUrl: './private-chat.component.scss'
 })
---
+++ DaBubble/src/app/components/shared/chat-messages-container/chat-messages-container.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chat-messages-container',
+  imports: [],
+  templateUrl: './chat-messages-container.component.html',
+  styleUrl: './chat-messages-container.component.scss'
+})
+export class ChatMessagesContainerComponent {
+
+}
