2b5a9a35a59941119a11ec1ac30ce9f5926189c3
andre <andre.karyoti.schuro@gmail.com>
2025-04-22 16:06:26 +0200

    added private-chat-header component & styles

--- DaBubble/src/app/components/private-chat/private-chat.component.ts
+++ DaBubble/src/app/components/private-chat/private-chat.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { ChatInputComponent } from '../shared/chat-input/chat-input.component';
 import { ChatMessagesContainerComponent } from '../shared/chat-messages-container/chat-messages-container.component';
+import { PrivateChatHeaderComponent } from '../shared/private-chat-header/private-chat-header.component';
 
 @Component({
   selector: 'app-private-chat',
   imports: [
     ChatInputComponent,
-    ChatMessagesContainerComponent
+    ChatMessagesContainerComponent,
+    PrivateChatHeaderComponent
   ],
   templateUrl: './private-chat.component.html',
   styleUrl: './private-chat.component.scss'
