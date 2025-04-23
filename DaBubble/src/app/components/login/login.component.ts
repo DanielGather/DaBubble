5bbfe3790092cd94b006cb1b09cc8d8546dd26dc
daberan <mail@davidberan.com>
2025-04-23 11:35:47 +0200

    implemented modular form-input components and updated login

--- DaBubble/src/app/components/login/login.component.ts
+++ DaBubble/src/app/components/login/login.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { ModalComponent } from '../shared/modal/modal.component';
+import { FormInputComponent } from '../shared/form-input/form-input.component';
 
 @Component({
   selector: 'app-login',
-  imports: [ModalComponent],
+  imports: [ModalComponent, FormInputComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
