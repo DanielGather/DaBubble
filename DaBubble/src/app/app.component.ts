4add69e30e9e19427684e96e187bb4b77ff55c62
andre <andre.karyoti.schuro@gmail.com>
2025-04-24 18:17:01 +0200

    added private chat to app-component as a test

--- DaBubble/src/app/app.component.ts
+++ DaBubble/src/app/app.component.ts
@@ -4,6 +4,8 @@ import { FirestoreTestComponent } from './components/firestore-test/firestore-te
 import { HeaderComponent } from './components/shared/header/header.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { AuthLayoutComponent } from './components/shared/auth-layout/auth-layout.component';
+import { PrivateChatComponent } from './components/private-chat/private-chat.component';
+
 
 @Component({
   selector: 'app-root',
@@ -13,6 +15,7 @@ import { AuthLayoutComponent } from './components/shared/auth-layout/auth-layout
     HeaderComponent,
     SidebarComponent,
     AuthLayoutComponent,
+    PrivateChatComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
