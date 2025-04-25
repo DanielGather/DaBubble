8c1bf29615786bde76b1a2de3c2c6365971ee6b8
daberan <mail@davidberan.com>
2025-04-25 16:53:41 +0200

    added microinteractions to choose avatar window

--- DaBubble/src/app/components/choose-avatar/choose-avatar.component.ts
+++ DaBubble/src/app/components/choose-avatar/choose-avatar.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
 import { ModalComponent } from '../shared/modal/modal.component';
 import { ButtonComponent } from '../shared/button/button.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-choose-avatar',
-  imports: [ModalComponent, ButtonComponent],
+  imports: [CommonModule, ModalComponent, ButtonComponent],
   templateUrl: './choose-avatar.component.html',
   styleUrl: './choose-avatar.component.scss',
 })
-export class ChooseAvatarComponent {}
+export class ChooseAvatarComponent {
+  currentAvatarSrc = '/img/avatar.svg';
+  avatars = [1, 2, 3, 4, 5, 6];
+  avatarIsSelected = false;
+
+  changeAvatar(avatarSrc: string) {
+    this.avatarIsSelected = true;
+    this.currentAvatarSrc = avatarSrc;
+    setTimeout(() => {
+      this.avatarIsSelected = false;
+    }, 100);
+  }
+}
