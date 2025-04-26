66dffe14a3153d048074377fa7084dfe87ba1a35
TypingAndy <andreas.georg@outlook.com>
2025-04-26 16:04:58 +0200

    started with routing, added layouts

---
+++ DaBubble/src/app/components/layouts/legals-layout/legals-layout.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-legals-layout',
+  imports: [],
+  templateUrl: './legals-layout.component.html',
+  styleUrl: './legals-layout.component.scss'
+})
+export class LegalsLayoutComponent {
+
+}
