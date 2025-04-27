af47001ca53692b8c5b6628740f0d8cf1c57530d
andre <andre.karyoti.schuro@gmail.com>
2025-04-27 23:24:20 +0200

    changed the layout so that oversroll-y is now on the message-field | added a function that scrolls to the end of the conversation on page loading

--- DaBubble/src/app/components/pages/main-page/chat-messages-container/chat-messages-container.component.ts
+++ DaBubble/src/app/components/pages/main-page/chat-messages-container/chat-messages-container.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, AfterViewInit } from '@angular/core';
+import { Component, Input, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 import { ChatMessage, ChatType, MessageType } from '../../../../types/types';
 
 
@@ -15,17 +15,37 @@ import { ChatMessageComponent } from './chat-message/chat-message.component';
   templateUrl: './chat-messages-container.component.html',
   styleUrl: './chat-messages-container.component.scss'
 })
-export class ChatMessagesContainerComponent implements AfterViewInit{
+export class ChatMessagesContainerComponent implements AfterViewInit {
   chatType = ChatType;
   messageType = MessageType;
+
+  /**
+   * is used to identify if the current chat is a private or a channel chat.
+   */
   @Input() chatTypeInput:ChatType = ChatType.default; 
+
+  /**
+   * this is the array that contains the chat messages
+   */
   @Input() chatMessages:Array<ChatMessage> = [];
+
+  /**
+   * an elementreference to the message field. (messages are rendered in here)
+   */
+  @ViewChild('messageField') private messageField!: ElementRef;
 
   //test
   userId = '123';
   //testends
 
   ngAfterViewInit(): void {
-    
+    this.scrollToBottom();
+  }
+
+/**
+ * is used to scroll to the bottom of the message container to see the newest messages
+ */
+  scrollToBottom():void {
+    this.messageField.nativeElement.scrollTop = this.messageField.nativeElement.scrollHeight;    
   }
 }
