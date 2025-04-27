76ae728dae0e3ca9503282ac3923fe9373735222
TypingAndy <andreas.georg@outlook.com>
2025-04-27 09:34:54 +0200

    auth design optimization

--- DaBubble/src/app/components/layouts/auth-layout/auth-layout.component.ts
+++ DaBubble/src/app/components/layouts/auth-layout/auth-layout.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { LegalLinksComponent } from '../../pages/auth/shared/legal-links/legal-links.component';
 import { RouterOutlet } from '@angular/router';
+import { DaBubbleLogoComponent } from '../../shared/da-bubble-logo/da-bubble-logo.component';
 
 @Component({
   selector: 'app-auth-layout',
-  imports: [
-LegalLinksComponent, RouterOutlet
-  ],
+  imports: [LegalLinksComponent, RouterOutlet, DaBubbleLogoComponent],
   templateUrl: './auth-layout.component.html',
   styleUrl: './auth-layout.component.scss',
 })
