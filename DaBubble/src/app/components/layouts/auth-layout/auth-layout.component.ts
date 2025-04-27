1f6ba88f0b94748511fd2adfd7b88c2130578a99
TypingAndy <andreas.georg@outlook.com>
2025-04-27 08:41:45 +0200

    optimizing layout

--- DaBubble/src/app/components/layouts/auth-layout/auth-layout.component.ts
+++ DaBubble/src/app/components/layouts/auth-layout/auth-layout.component.ts
@@ -1,25 +1,11 @@
 import { Component } from '@angular/core';
-import { ButtonComponent } from '../../shared/button/button.component';
-import { ChooseAvatarComponent } from '../../pages/auth/choose-avatar/choose-avatar.component';
-import { LoginComponent } from '../../pages/auth/login/login.component';
-import { ResetPasswordComponent } from '../../pages/auth/reset-password/reset-password.component';
-import { SetPasswordComponent } from '../../pages/auth/set-password/set-password.component';
-import { SignupComponent } from '../../pages/auth/signup/signup.component';
-import { RouterLink, RouterOutlet } from '@angular/router';
-import { HeaderComponent } from '../../shared/header/header.component';
+import { LegalLinksComponent } from '../../pages/auth/shared/legal-links/legal-links.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-auth-layout',
   imports: [
-    LoginComponent,
-    ButtonComponent,
-    SignupComponent,
-    ChooseAvatarComponent,
-    ResetPasswordComponent,
-    SetPasswordComponent,
-    RouterLink,
-    RouterOutlet,
-    HeaderComponent,
+LegalLinksComponent, RouterOutlet
   ],
   templateUrl: './auth-layout.component.html',
   styleUrl: './auth-layout.component.scss',
