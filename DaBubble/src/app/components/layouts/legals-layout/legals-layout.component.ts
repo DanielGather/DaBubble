e22f410c3117135218d9f9c3b967018191539c47
TypingAndy <andreas.georg@outlook.com>
2025-04-27 09:09:56 +0200

    more design refactoring

--- DaBubble/src/app/components/layouts/legals-layout/legals-layout.component.ts
+++ DaBubble/src/app/components/layouts/legals-layout/legals-layout.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { DaBubbleLogoComponent } from '../../shared/da-bubble-logo/da-bubble-logo.component';
 
 @Component({
   selector: 'app-legals-layout',
-  imports: [CommonModule, RouterLink, RouterOutlet],
+  imports: [CommonModule, RouterLink, RouterOutlet, DaBubbleLogoComponent],
   templateUrl: './legals-layout.component.html',
   styleUrl: './legals-layout.component.scss'
 })
--- DaBubble/src/app/components/pages/auth/set-password/set-password.component.ts
+++ DaBubble/src/app/components/pages/auth/set-password/set-password.component.ts
@@ -3,10 +3,11 @@ import { ButtonComponent } from '../../../shared/button/button.component';
 import { FormInputComponent } from '../../../shared/form-input/form-input.component';
 import { ModalComponent } from '../../../shared/modal/modal.component';
 import { LegalLinksComponent } from '../shared/legal-links/legal-links.component';
+import { DaBubbleLogoComponent } from '../../../shared/da-bubble-logo/da-bubble-logo.component';
 
 @Component({
   selector: 'app-set-password',
-  imports: [ButtonComponent, FormInputComponent, ModalComponent, LegalLinksComponent],
+  imports: [ButtonComponent, FormInputComponent, ModalComponent, LegalLinksComponent, DaBubbleLogoComponent],
   templateUrl: './set-password.component.html',
   styleUrl: './set-password.component.scss',
 })
