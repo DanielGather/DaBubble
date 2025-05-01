e1bb86838101132588090201a627f3e8402f9938
TypingAndy <andreas.georg@outlook.com>
2025-05-01 11:52:33 +0200

    channel edit integration done

--- DaBubble/src/app/components/pages/main-page/channel-edit-popup/channel-edit-popup.component.ts
+++ DaBubble/src/app/components/pages/main-page/channel-edit-popup/channel-edit-popup.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { DropdownComponent } from '../shared/dropdown/dropdown.component';
 import { ButtonComponent } from '../../../shared/button/button.component';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-channel-edit-popup',
-  imports: [DropdownComponent, ButtonComponent],
+  imports: [DropdownComponent, ButtonComponent, FormsModule, CommonModule],
   templateUrl: './channel-edit-popup.component.html',
-  styleUrl: './channel-edit-popup.component.scss'
+  styleUrl: './channel-edit-popup.component.scss',
 })
 export class ChannelEditPopupComponent {
+  @Input() visible: boolean = false; // 👈 DAS ist wichtig
+  @Output() closed = new EventEmitter<void>();
 
+  onCloseClick() {
+    this.closed.emit();
+  }
 }
