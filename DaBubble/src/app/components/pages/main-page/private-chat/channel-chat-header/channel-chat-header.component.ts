46bf6db14c949a9d0d368c7209bd938a3a61b146
Loew3nzahn <gather.daniel@yahoo.de>
2025-05-01 10:23:34 +0200

    added w-full to channel chat header

--- DaBubble/src/app/components/pages/main-page/private-chat/channel-chat-header/channel-chat-header.component.ts
+++ DaBubble/src/app/components/pages/main-page/private-chat/channel-chat-header/channel-chat-header.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { ButtonComponent } from '../../../../shared/button/button.component';
 
 @Component({
   selector: 'app-channel-chat-header',
-  imports: [],
+  imports: [ButtonComponent],
   templateUrl: './channel-chat-header.component.html',
-  styleUrl: './channel-chat-header.component.scss'
+  styleUrl: './channel-chat-header.component.scss',
 })
-export class ChannelChatHeaderComponent {
-
-}
+export class ChannelChatHeaderComponent {}
