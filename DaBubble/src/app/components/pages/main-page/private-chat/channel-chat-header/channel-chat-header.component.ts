0dfdfa4de8fa7a5ee9d08fa9a65a94b065452d40
TypingAndy <andreas.georg@outlook.com>
2025-05-01 11:16:52 +0200

    channel edit popup integation, not done

--- DaBubble/src/app/components/pages/main-page/private-chat/channel-chat-header/channel-chat-header.component.ts
+++ DaBubble/src/app/components/pages/main-page/private-chat/channel-chat-header/channel-chat-header.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from '../../../../shared/button/button.component';
+import { ChannelEditPopupComponent } from '../../channel-edit-popup/channel-edit-popup.component';
+import { DropdownComponent } from '../../shared/dropdown/dropdown.component';
 
 @Component({
   selector: 'app-channel-chat-header',
-  imports: [ButtonComponent],
+  imports: [ButtonComponent, ChannelEditPopupComponent, DropdownComponent],
   templateUrl: './channel-chat-header.component.html',
   styleUrl: './channel-chat-header.component.scss',
 })
