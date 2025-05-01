443a17cf0dd8bcdccbf2477f50a4e86f0ebf614b
andre <andre.karyoti.schuro@gmail.com>
2025-05-01 22:37:09 +0200

    added pop-over komponent | chnanged every menu in chat-message komponent to pop-over

--- DaBubble/src/app/components/pages/main-page/chat-main/chat-messages-container/chat-message/chat-message.component.ts
+++ DaBubble/src/app/components/pages/main-page/chat-main/chat-messages-container/chat-message/chat-message.component.ts
@@ -2,12 +2,14 @@ import { Component, Input } from '@angular/core';
 import { MessageType, ChatMessage, ChatType} from '../../../../../../types/types';
 import { SingleEmojiComponent } from './single-emoji/single-emoji.component';
 import { CommonModule } from '@angular/common';
+import { PopOverComponent } from '../../../shared/pop-over/pop-over.component';
 
 @Component({
   selector: 'app-chat-message',
   imports: [
     SingleEmojiComponent,
-    CommonModule
+    CommonModule,
+    PopOverComponent
   ],
   templateUrl: './chat-message.component.html',
   styleUrl: './chat-message.component.scss'
