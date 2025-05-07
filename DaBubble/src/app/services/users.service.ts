9640474b437549a9f4d0c36519e48b6e9f562412
Loew3nzahn <gather.daniel@yahoo.de>
2025-05-07 09:17:55 +0200

    added getUserData function to firestoreservice

--- DaBubble/src/app/services/users.service.ts
+++ DaBubble/src/app/services/users.service.ts
@@ -8,21 +8,18 @@ import { UserCredential } from 'firebase/auth';
   providedIn: 'root',
 })
 export class UsersService {
-
-  
-  currentUserCredential:UserCredential|null = null;
-  currentUserId:string|null = null;
-  currentUser:AppUser|null = null;
-
+  currentUserCredential: UserCredential | null = null;
+  currentUserId: string | null = null;
+  currentUser: AppUser | null = null;
 
   private currentUserSubject = new BehaviorSubject<AppUser | null>(null);
+
   readonly currentUser$ = this.currentUserSubject.asObservable();
   firestoreService: FirestoreService = inject(FirestoreService);
 
   readonly usersList$: Observable<AppUser[]> =
     this.firestoreService.getCollectionData('users') as Observable<AppUser[]>;
-  
-  
+
   constructor() {}
 
   setCurrentUser(user: AppUser | null) {
