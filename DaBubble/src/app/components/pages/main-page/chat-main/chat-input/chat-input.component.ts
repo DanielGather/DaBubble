354c246be0ee4b528476facb0c4b700d6bef8998
andre <andre.karyoti.schuro@gmail.com>
2025-05-19 17:34:23 +0200

    added funktionality to get-url-chatid.service.ts | added funktionality of message input, now it can create new chat docs with the right ids

--- DaBubble/src/app/components/pages/main-page/chat-main/chat-input/chat-input.component.ts
+++ DaBubble/src/app/components/pages/main-page/chat-main/chat-input/chat-input.component.ts
@@ -1,7 +1,13 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit, HostListener } from '@angular/core';
+import { Component, OnInit, OnDestroy, HostListener, inject, Input } from '@angular/core';
 import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
-
+import { UsersService } from '../../../../../services/users.service';
+import { ChatType } from '../../../../../types/types';
+import { GetUrlChatidService } from '../../../../../services/get-url-chatid.service';
+import { FirestoreService } from '../../../../../services/firestore.service';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+import { ChannelsService } from '../../../../../services/channels.service';
 
 
 @Component({
@@ -14,12 +20,34 @@ import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
   templateUrl: './chat-input.component.html',
   styleUrl: './chat-input.component.scss',
 })
-export class ChatInputComponent implements OnInit {
+export class ChatInputComponent implements OnInit, OnDestroy {
+  //services
+  usersService = inject(UsersService);
+  urlService = inject(GetUrlChatidService);
+  firestoreService = inject(FirestoreService);
+  channelService = inject(ChannelsService);
+
+  //unsubscribe variables
+  private destroy$ = new Subject<void>();
+
+  //state booleans
   showEmojiMenu: boolean = false;
 
+  //chattype
+  chatType: ChatType = ChatType.default;
 
+  //data storage variables
+  channelData: any;
+
+  //formgroup
   chatInputGroup = new FormGroup({
-    chatInput: new FormControl('')
+    channelId: new FormControl(''),
+    privatChatId: new FormControl(''),
+    threadsId: new FormControl(''),
+    creatorId: new FormControl(''),
+    timestamp: new FormControl(0),
+    message: new FormControl(''),
+    userIds: new FormControl()
   })
 
   @HostListener('document:click', ['$event'])
@@ -35,13 +63,70 @@ export class ChatInputComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.setCreatorIdOfMessageObject();
+    this.setChatIdOfMessageObject();
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 
   onSubmit() {
     if (this.chatInputGroup.invalid) {
       console.warn("Form ist ungültig, sende trotzdem.");
     }
-    console.log("Absenden:", this.chatInputGroup.value);
+    else if (this.usersService.currentUserId && this.chatInputGroup.valid && this.urlService.currentParams.chatType != null) {
+      this.chatInputGroup.get('timestamp')?.setValue(new Date().getTime());
+      this.addNewMessageToFirestoreCollection();
+      console.log("Absenden:", this.chatInputGroup.value);
+    }
+  }
 
+  setChatIdOfMessageObject() {
+    this.urlService.urlParameter$
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(params => {
+        if (params.chatType && params.chatId) {
+          switch (params.chatType) {
+            case 'private':
+              this.chatInputGroup.get('privatChatId')?.setValue(params.chatId);
+              break;
+
+            case 'channel':
+              this.chatInputGroup.get('channelId')?.setValue(params.chatId);
+              break;
+
+            default:
+              break;
+          }
+
+          if (params.chatType === 'channel' && params.chatId) {
+            this.channelService.getChannelById$(params.chatId)
+              .pipe(takeUntil(this.destroy$))
+              .subscribe(channel => {
+                this.channelData = channel;
+                this.chatInputGroup.get('userIds')?.setValue(channel.userIds);
+              });
+          }
+
+        }
+      })
+  }
+
+  setCreatorIdOfMessageObject() {
+    this.usersService.currentUser$
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(user => {
+        if (user) {
+          this.chatInputGroup.get('creatorId')?.setValue(user.userId!);
+        }
+      });
+  }
+
+  addNewMessageToFirestoreCollection() {
+    let formData = this.chatInputGroup.getRawValue();
+    this.firestoreService.addDoc('messages', formData)
+      .then(() => { this.chatInputGroup.get('message')?.reset(); });
   }
 }
--- DaBubble/src/app/services/get-url-chatid.service.ts
+++ DaBubble/src/app/services/get-url-chatid.service.ts
@@ -1,10 +1,51 @@
-import { Injectable} from '@angular/core';
+import { Injectable } from '@angular/core';
+import { Router, NavigationEnd, ActivatedRouteSnapshot } from '@angular/router';
+import { BehaviorSubject, filter } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GetUrlChatidService {
-  public currentChatID: string = '';
+private urlParameterSubject = new BehaviorSubject<{
+    chatType: string | null;
+    chatId: string | null;
+    threadsId?: string | null;
+  }>({
+    chatType: null,
+    chatId: null,
+    threadsId: null,
+  });
 
-  constructor() {}
+  readonly urlParameter$ = this.urlParameterSubject.asObservable();
+
+  constructor(private router: Router) {
+    this.router.events
+      .pipe(filter((e): e is NavigationEnd => e instanceof NavigationEnd))
+      .subscribe(() => {
+        const snapshot = this.router.routerState.snapshot.root;
+        const routeParams = this.findDeepestChildWithParams(snapshot);
+
+        const chatType = routeParams?.params['chatType'] || null;
+        const chatId = routeParams?.params['id'] || null;
+        const threadsId = routeParams?.params['threadsId'] || null; // falls später ergänzt
+
+        this.urlParameterSubject.next({
+          chatType,
+          chatId,
+          threadsId
+        });
+      });
+  }
+
+  // Rekursiv zur tiefsten Route mit Params
+  private findDeepestChildWithParams(route: ActivatedRouteSnapshot): ActivatedRouteSnapshot | null {
+    while (route.firstChild) {
+      route = route.firstChild;
+    }
+    return route;
+  }
+
+  get currentParams() {
+    return this.urlParameterSubject.getValue();
+  }
 }
